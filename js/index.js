f3932f2aba8c60a2e620396ea217fd5ffdf2da41
Naman Gogia <namangogia2001@gmail.com>
2021-09-26 21:03:33 +0530

    Type conv: String() Number() parseInt() parseFloat() toFixed() Type coercion String + Number

--- js/index.js
+++ js/index.js
@@ -1,45 +1,44 @@
-/* Data types in js
-1. Primitive data types:
-    Memory allocation in stack
-    Strings, numbers, boolean, null, undefined, symbol
-        null is set by you to check
-        undefined is default value
+/* Type conversion and type coercion */
+let myVar, boolVar;
 
-2. Reference data types:
-    Memory allocation in heap
-    Arrays, object literals, functions, dates
+myVar = String(34);
+boolVar = String(true);
+
+console.log(myVar, typeof myVar); // 34 string
+console.log(boolVar, typeof boolVar); // true string
+
+let date = String(new Date());
+console.log(date); // Sun Sep 26 2021 20:48:46 GMT+0530 (India Standard Time)
+
+let arr = String([1, 2, 3]);
+console.log([arr, arr.length]); // (2) ['1,2,3', 5]
+
+// String() can be replaced with myVar.toString()
+
+let stri = Number("123");
+console.log(stri, typeof stri); // 123 'number'
+
+stri = Number("1a");
+console.log(stri, typeof stri); // NaN 'number'
+
+stri = Number(true);
+console.log(stri, typeof stri); // 1 'number'
+
+stri = parseInt("123.123");
+console.log(stri, typeof stri); // 123 'number'
+
+stri = parseFloat("123.123");
+console.log(stri, typeof stri); // 123.123 'number'
+
+// Number() encompasses both parseInt and parseFloat
+
+console.log(stri.toFixed(1)); // 123.1
+
+/* Type coercion
+String + Number == String + String
 */
 
-// Primitive DT
-
-console.log(typeof "Naman"); // string
-
-let marks = 100;
-console.log(typeof marks); // number
-
-let nullVar = null;
-console.log(typeof nullVar); // object. But it is a primitive data type. So it has a bogus return value
-
-let undef = undefined;
-console.log(typeof undef); // undefined
-
-// Reference DT
-
-// Arrays
-let arr = [1, 2, 3, false, "naman"];
-console.log(typeof arr); // object
-
-// Object Literals
-let students = {
-  alice: 80,
-  bob: 85,
-  charlie: 90,
-};
-console.table(students); // object
-console.log(typeof students);
-
-function findName() {}
-console.log(typeof findName); // function
-
-let date = new Date();
-console.log(typeof date); // object
+stri = "123";
+myVar = 12;
+let ans = stri + myVar;
+console.log(ans, typeof ans);
