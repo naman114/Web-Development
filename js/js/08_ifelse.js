13a59115b217dd1b0e32c095919611c5025592f1
Naman Gogia <namangogia2001@gmail.com>
2021-09-27 00:36:36 +0530

    conditionals and loops

---
+++ js/js/08_ifelse.js
@@ -0,0 +1,18 @@
+/* If else, switch */
+
+let age = 12;
+
+// Check if a variable has been defined or not
+if (typeof age !== "undefined") {
+  console.log("Defined");
+} else {
+  console.log("Undefined");
+}
+
+// Any non zero value is true like C++
+let i = 0;
+if (i) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
