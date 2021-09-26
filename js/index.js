c9b10f8c9c67750e4894ee9e46afbb79cad73806
Naman Gogia <namangogia2001@gmail.com>
2021-09-26 20:32:38 +0530

    Primitive and reference data types

--- js/index.js
+++ js/index.js
@@ -1,33 +1,45 @@
-// Variables: var, let, const
+/* Data types in js
+1. Primitive data types:
+    Memory allocation in stack
+    Strings, numbers, boolean, null, undefined, symbol
+        null is set by you to check
+        undefined is default value
 
-// const
-/* Invalid: Missing initializer in const declaration
-const name; */
-
-// Cannot modify hereinafter
-const name = "Naman";
-// Irrespective of const, arrays and objects can be modified using their functions but cannot be reassigned
-const arr = [1, 2, 3];
-// Valid
-arr.push(4);
-console.log(arr);
-// Invalid
-// arr = [5, 6];
-
-// let: block level scope. var has global scope
-var city = "goa";
-{
-  let city = "delhi";
-  console.log({ city });
-}
-console.log({ city });
-
-// let and const when used together remove the need to use var. let variables stay in block so easier to read and maintain the code
-
-/* 
-Commonly used cases
-1. PascalCase
-2. camelCase
-3. kebab-case
-4. snake_case
+2. Reference data types:
+    Memory allocation in heap
+    Arrays, object literals, functions, dates
 */
+
+// Primitive DT
+
+console.log(typeof "Naman"); // string
+
+let marks = 100;
+console.log(typeof marks); // number
+
+let nullVar = null;
+console.log(typeof nullVar); // object. But it is a primitive data type. So it has a bogus return value
+
+let undef = undefined;
+console.log(typeof undef); // undefined
+
+// Reference DT
+
+// Arrays
+let arr = [1, 2, 3, false, "naman"];
+console.log(typeof arr); // object
+
+// Object Literals
+let students = {
+  alice: 80,
+  bob: 85,
+  charlie: 90,
+};
+console.table(students); // object
+console.log(typeof students);
+
+function findName() {}
+console.log(typeof findName); // function
+
+let date = new Date();
+console.log(typeof date); // object
