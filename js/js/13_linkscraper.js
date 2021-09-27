0540f710dc569bbf1340eb109c89841ca3f81762
Naman Gogia <namangogia2001@gmail.com>
2021-09-27 17:50:55 +0530

    Link scraper

--- js/js/13_linkscraper.js
+++ js/js/13_linkscraper.js
@@ -1,3 +1,4 @@
+// Paste in console of any website to find links having the string in its innerHTML
 Array.from(document.links).forEach(function (ele) {
   let s = ele.innerHTML;
   if (s.includes("Project")) {
