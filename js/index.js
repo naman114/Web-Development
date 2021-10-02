a3f36bd9c03cd5887ec0741552cdb4e53298c588
naman114 <namangogia2001@gmail.com>
2021-10-02 21:09:36 +0530

    Promises

--- js/index.js
+++ js/index.js
@@ -1,44 +1,25 @@
-/* Callback functions
+/* Promises
 
-// forEach is a synchronous function
-arr.forEach(function(){});
-
-// setTimeout is an asynchronous function that executes the given function after its timer expires
-
+States:
+- resolve
+- reject
+- pending
 */
 
-let students = [
-  {
-    name: "Alice",
-    city: "New Delhi",
-  },
-  {
-    name: "Bob",
-    city: "Hyderabad"
-  }
-];
-
-function enrol(student, callback){
-  setTimeout(
-    // Function that executes after the timer expires
-    function(){
-    students.push(student);
-    console.log(`${student.name} has been enrolled`);
-    callback();
-  }, 8000);
-};
-
-function display(){
-  let list = document.getElementById("list");
-  let str = "";
-  setTimeout(() => {
-    students.forEach(function(student){
-      str += `<li>${student.name}</li>`;
-    });
-    list.innerHTML = str;
-    console.log("Students have been displayed");
-  }, 1000);
+function func(){
+  return new Promise(function(resolve, reject){
+    setTimeout(() => {
+      const error = true;
+      if(!error){
+        console.log("Function: Promise resolved");
+        resolve();
+      }
+      else{
+        console.log("Function: Promise not resolved");
+        reject("Return value: Not resolved");
+      }
+    }, 2000);
+  });
 }
 
-enrol({name: "Charlie", city: "Shimla"}, display);
-
+func().then(function(){console.log("Then: Done")}).catch(function(){console.log("Not done")});
