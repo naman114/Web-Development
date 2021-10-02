078262638481964ce1b0a5138992ff3e226f0c89
naman114 <namangogia2001@gmail.com>
2021-10-02 19:09:01 +0530

    AJAX contd.

--- js/index.js
+++ js/index.js
@@ -22,7 +22,6 @@ Working:
 */
 
 let fetchBtn = document.getElementById("fetchBtn");
-
 fetchBtn.addEventListener("click", buttonClickHandler);
 
 function buttonClickHandler() {
@@ -32,22 +31,71 @@ function buttonClickHandler() {
 
   // Open the object
   // Request type, source of data, true for asynchronous nature/ non blocking nature
-  xhr.open("GET", "datasrc.txt", true);
+  // xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+
+  // POST REQUEST
+  xhr.open("POST", "https://reqres.in/api/users", true);
+  xhr.getResponseHeader("Content-type", "application/json");
 
   // What to do during progress (optional)
   xhr.onprogress = function () {
     console.log("On progress");
   };
 
-  //   What to do when response is ready
+  xhr.onreadystatechange = function(){
+    console.log("Ready state is: " + xhr.readyState);
+  }
+  /* 
+  0: Unsent
+  1: Opened
+  2: Headers Received
+  3: Loading
+  4: Done
+   */
+
+  //   What to do when response is ready (same as ready state = 4)
+  xhr.onload = function () {
+    // 201 means created
+    if (this.status === 201) {
+      console.log(this.responseText);
+    } else {
+      console.error("Some error occurred: " + this.status);
+    }
+  };
+
+  // Send the request. The above behavior is defined for after the request is sent (Bheju to ye karna)
+  let params = 	`{
+    "name": "morpheus",
+    "job": "leader",
+    "id": "260",
+    "createdAt": "2021-10-02T13:17:02.885Z"
+}`;
+  xhr.send(params);
+}
+
+let popBtn = document.getElementById("popBtn");
+popBtn.addEventListener("click", popHandler);
+
+function popHandler(){
+  console.log("Pop handler was clicked");
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://reqres.in/api/users?page=2", true);
+
   xhr.onload = function () {
     if (this.status === 200) {
-      console.log(this.responseText);
+      let res = JSON.parse(this.responseText);
+      console.log(res.data);
+      
+      let list = document.getElementById("list");
+      res.data.forEach(function(ele){
+        list.innerHTML += `<li>${ele.email}</li>`;
+      });
     } else {
       console.error("Some error occurred");
     }
   };
 
-  //   Send the request
   xhr.send();
 }
