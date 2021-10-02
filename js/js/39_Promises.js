999df5886b27fd7123f9632524e47051c27ab13d
naman114 <namangogia2001@gmail.com>
2021-10-02 21:10:32 +0530

    Promises file

---
+++ js/js/39_Promises.js
@@ -0,0 +1,25 @@
+/* Promises
+
+States:
+- resolve
+- reject
+- pending
+*/
+
+function func(){
+    return new Promise(function(resolve, reject){
+      setTimeout(() => {
+        const error = true;
+        if(!error){
+          console.log("Function: Promise resolved");
+          resolve();
+        }
+        else{
+          console.log("Function: Promise not resolved");
+          reject("Return value: Not resolved");
+        }
+      }, 2000);
+    });
+  }
+  
+  func().then(function(){console.log("Then: Done")}).catch(function(){console.log("Not done")});
