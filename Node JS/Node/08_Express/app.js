c7f716c06aad24f5f0ffaa825f38042aac970bfc
naman114 <namangogia2001@gmail.com>
2021-10-17 16:24:17 +0530

    Serving static files using pug

--- Node JS/Node/08_Express/app.js
+++ Node JS/Node/08_Express/app.js
@@ -1,10 +1,28 @@
 console.log("This is app.js");
 
 const express = require("express");
+const path = require("path");
 
 const app = express(); // Created an express app
 
 const port = 80;
+
+/* Static files are those which are publicly available for a user to access without logging in */
+/* Use `code .` to open a VS Code session of just one directory */
+/* pug is a template engine */
+
+app.use("/static", express.static("static"));
+
+// Set the template engine aas pug
+app.set("view engine", "pug");
+
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
+
+// Pug demo endpoint
+app.get("/demo", function (req, res) {
+  res.status(200).render("demo", { title: "Hey", message: "Hello there!" });
+});
 
 app.get("/", (req, res) => {
   res.send("This is the home page");
