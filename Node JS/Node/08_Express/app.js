c23dbbf41331bf69c81048c31a1d7fe77ef63976
naman114 <namangogia2001@gmail.com>
2021-10-21 19:05:59 +0530

    Parsing form data and storing it to a file

--- Node JS/Node/08_Express/app.js
+++ Node JS/Node/08_Express/app.js
@@ -1,6 +1,7 @@
 console.log("This is app.js");
 
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 
 const app = express(); // Created an express app
@@ -13,6 +14,8 @@ const port = 80;
 
 // EXPRESS RELATED CONFGS
 app.use("/static", express.static("static"));
+// We need a middleware to get the data sent by the form. The statement below helps the form data to raech express
+app.use(express.urlencoded());
 
 // PUG RELATED CONFGS
 app.set("view engine", "pug"); // Set the template engine aas pug
@@ -21,10 +24,30 @@ app.set("views", path.join(__dirname, "views")); // Set the views directory
 // ENDPOINTS
 app.get("/", (req, res) => {
   const params = {
-    title: "Hello from params",
-    content: "Plain HTML using Pug (from params)",
+    title: "GainsBuddy | Join Now",
+    content:
+      "Get the best personal training with cutting-edge equipment at just 1499/- monthly. Sign up now!",
   };
   res.status(200).render("index.pug", params);
+});
+
+app.post("/", (req, res) => {
+  // The returned object will have the keys as the name parameters in the input and textarea tags
+  console.log(req.body);
+
+  let name = req.body.name;
+  let age = req.body.age;
+  let gender = req.body.gender;
+  let address = req.body.address;
+  let additional = req.body.additional;
+  let outputToWrite = `${name}, ${age}, ${gender}, ${address}, ${additional}\n`;
+
+  fs.appendFileSync("userData.txt", outputToWrite);
+
+  const params = {
+    message: "Your data has been recorded successfully",
+  };
+  res.status(201).render("index.pug", params);
 });
 
 // START THE SERVER
