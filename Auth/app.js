fb2b9932a6daf040bb41ac8b5db57b99a5089aac
naman114 <namangogia2001@gmail.com>
2021-10-27 20:23:54 +0530

    DB connected, login and register routes working

--- Auth/app.js
+++ Auth/app.js
@@ -9,6 +9,19 @@ app.set("view engine", "pug");
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 
+// Mongoose
+mongoose.connect("mongodb://localhost:27017/users");
+
+let User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  })
+);
+
 // Endpoints
 app.get("/", (req, res) => {
   res.render("index");
@@ -19,11 +32,37 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  res.json(req.body);
+  let user = new User(req.body);
+
+  user.save((err) => {
+    if (err) {
+      let error = "Something went wrong. Please try again.";
+
+      if (err.code === 11000) {
+        error = "Email is already taken, try with another one";
+      }
+
+      return res.render("register", { error: error });
+    }
+
+    return res.redirect("login");
+  });
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err || !user || req.body.password !== user.password) {
+      return res.render("login", {
+        error: "Incorrect email/password",
+      });
+    }
+
+    return res.redirect("dashboard");
+  });
 });
 
 app.get("/dashboard", (req, res) => {
