6909ca8cc9b65b210f95fdb438a61e729de6e05e
Naman Gogia <namangogia2001@gmail.com>
2022-01-23 21:03:37 +0530

    Add python server with dynamic routing

--- Django/WD201/Level-3/server.py
+++ Django/WD201/Level-3/server.py
@@ -1,11 +1,27 @@
 # importing from the same module from which we created the simple directory listing server
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from datetime import date, datetime
 
 # The base class is the one that was rendering the directory listing page
 class MyServer(SimpleHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        # The following line says that we'll be sending text/html type data
 
-address = "127.0.0.1" #localhost
+        if self.path == "/hello":
+            self.send_response(200)
+            self.send_header("content-type", "text/html")
+            content = f"<h1>Hello! {datetime.now()} </h1>"
+        else:
+            self.send_response(404)
+            self.send_header("content-type", "text/html")
+            content = "<h1>Error</h1>"
+
+        # This function is called when all the metadata (data that is not rendered) is sent
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+
+address = "127.0.0.1"  # localhost
 port = 8000
 server_address = (address, port)
 
@@ -13,5 +29,3 @@ server_address = (address, port)
 httpd = HTTPServer(server_address, MyServer)
 # Serve till we stop the program
 httpd.serve_forever()
-
-# Keep in mind that this will work the same as the simple http server since we have not added any new functionality to our derived class
