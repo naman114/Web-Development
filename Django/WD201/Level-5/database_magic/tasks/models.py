d0cfac424947571f84f55d4e66ad8d687709c450
Naman Gogia <namangogia2001@gmail.com>
2022-01-31 00:54:55 +0530

    Apps, models and migrations

--- Django/WD201/Level-5/database_magic/tasks/models.py
+++ Django/WD201/Level-5/database_magic/tasks/models.py
@@ -5,3 +5,4 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now=True)
