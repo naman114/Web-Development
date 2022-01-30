a5eff44f33fee82bef8355b755ffccd01d586660
Naman Gogia <namangogia2001@gmail.com>
2022-01-31 01:04:49 +0530

    Adding templates and views

--- Django/WD201/Level-5/database_magic/database_magic/urls.py
+++ Django/WD201/Level-5/database_magic/database_magic/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path
 
+from tasks.views import (
+    tasks_view,
+    completed_view,
+    all_tasks_view,
+    add_task_view,
+    complete_task_view,
+    delete_task_view,
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("tasks/", tasks_view),
+    path("completed_tasks/", completed_view),
+    path("all_tasks/", all_tasks_view),
+    path("add-task/", add_task_view),
+    path("complete_task/<int:index>/", complete_task_view),
+    path("delete-task/<int:index>/", delete_task_view),
 ]
--- Django/WD201/Level-5/database_magic/tasks/views.py
+++ Django/WD201/Level-5/database_magic/tasks/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+active_tasks = []
+completed_tasks = []
+
+# Pending tasks
+def tasks_view(request):
+    return render(request, "pending_tasks.html", {"tasks": active_tasks})
+
+
+# Completed tasks
+def completed_view(request):
+    return render(request, "completed_tasks.html", {"tasks": completed_tasks})
+
+
+# All tasks
+def all_tasks_view(request):
+    return render(
+        request,
+        "all_tasks.html",
+        {"active_tasks": active_tasks, "completed_tasks": completed_tasks},
+    )
+
+
+# Add a task
+def add_task_view(request):
+    task_to_add = request.GET.get("task")
+    active_tasks.append(task_to_add)
+    return HttpResponseRedirect("/tasks")
+
+
+# Delete a task
+def delete_task_view(request, index):
+    if index <= len(active_tasks):
+        del active_tasks[index - 1]
+    return HttpResponseRedirect("/tasks")
+
+
+# Mark task as complete
+def complete_task_view(request, index):
+    if index <= len(active_tasks):
+        completed_tasks.append(active_tasks[index - 1])
+        del active_tasks[index - 1]
+    return HttpResponseRedirect("/tasks")
