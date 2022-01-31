65dad6f8c4458912ee42365fa9ae4acd447a1335
Naman Gogia <namangogia2001@gmail.com>
2022-01-31 20:41:03 +0530

    Adding foreign key

--- Django/WD201/Level-5/database_magic/tasks/models.py
+++ Django/WD201/Level-5/database_magic/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
@@ -7,6 +8,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
