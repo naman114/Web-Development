e4e059d48e147f51e502f9ef9c22390556a1082e
Naman Gogia <namangogia2001@gmail.com>
2022-03-13 02:13:52 +0530

    Nesting of react components

---
+++ ReactJS/WD-301/my-app/src/AppContainer.tsx
@@ -0,0 +1,10 @@
+// Parent Wrapper Component
+import React from "react";
+
+export default function AppContainer(props: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen bg-gray-100 items-center">
+      {props.children}
+    </div>
+  );
+}
