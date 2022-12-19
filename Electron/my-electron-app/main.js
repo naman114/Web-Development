5e11049765a34311fd950f6b708fcf8f7fad66b9
Naman Gogia <namangogia2001@gmail.com>
2022-12-19 08:53:31 +0530

    Hello Electron

---
+++ Electron/my-electron-app/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, mainWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // Mac apps continue running on closing all windows.
+  // In that case, if a user opens the app again, it is still in ready state i.e. no promise to resolve
+  // So we need to listen for activate event and create a window in case none are open
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
