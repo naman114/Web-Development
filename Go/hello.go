65810bf5784bbbce8dc815b5f758ce129a780f55
Naman Gogia <ngogia@atlassian.com>
2023-07-30 01:12:53 +0530

    Hello Go

---
+++ Go/hello.go
@@ -0,0 +1,14 @@
+/*
+	Start by go mod init example/hello 
+*/
+
+package main // package is a way to group functions. Consists of all files in the same directory
+
+import "fmt" // Standard library package
+
+// main function executes bty default when you run the main package
+// go run .
+// go help -> get all commands
+func main() {
+    fmt.Println("Hello, World!")
+}
