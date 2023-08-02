5c24f41b091898d4b564b1078459e1d06fe820c6
Naman Gogia <ngogia@atlassian.com>
2023-08-02 15:21:54 +0530

    feat: external go modules

--- Go/hello.go
+++ Go/hello.go
@@ -5,10 +5,11 @@
 package main // package is a way to group functions. Consists of all files in the same directory
 
 import "fmt" // Standard library package
+import "rsc.io/quote" // To locate and download the latest version of new packages -> go mod tidy
 
 // main function executes bty default when you run the main package
 // go run .
 // go help -> get all commands
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println(quote.Go())
 }
